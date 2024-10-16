a2e3dd6d43333fde91e79dfcf4e2f9e1a1c86e34
val <ginerv@yahoo.com>
2024-10-15 23:28:41 -0500

    Separated parameters into a new python file settings.py.  Tested with it.

---
+++ settings.py
@@ -0,0 +1,18 @@
+# settings.py
+
+file_path = './yc_startups.json'
+model_name = 'all-MiniLM-L6-v2'
+local_language_model_name = 'gpt2'
+
+prompt_messages = [
+        ("system", """
+        You are an assistant for question-answering tasks.
+        Use the following pieces of retrieved context to answer the question.
+        If you don't know the answer, just say that you don't know.
+        Use three sentences maximum and keep the answer concise.
+        Question: {question}
+        Context: {context}
+        Answer:""")
+    ]
+
+question = "Generate a company idea for the HR industry based on provided context"
